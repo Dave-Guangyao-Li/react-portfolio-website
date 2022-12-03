2afb1b34ac3902fc92952ed7cbb3ec9c3bf8bd64
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2022-12-02 16:16:44 -0800

    import assets and introduction page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,27 @@
-import "./Intro.scss"
+import './Intro.scss'
 
 export default function Intro() {
   return (
-    <div className='intro' id="intro">Intro</div>
+    <div className='intro' id='intro'>
+      <div className='left'>
+        <div className='img-container'>
+          <img src='assets/man.png' alt='profile-pic' />
+        </div>
+      </div>
+
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hi There, I'm</h2>
+          <h1>John Doe</h1>
+          <h3>
+            Developer<span></span>
+          </h3>
+        </div>
+
+        <a href='#portfolio'>
+          <img src='assets/down.png' alt='down-arrow-pic' />
+        </a>
+      </div>
+    </div>
   )
 }
