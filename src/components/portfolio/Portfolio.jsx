37dad045528f6b777d5744550a31e59e620a5448
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2022-12-06 09:23:33 -0800

    portfolio page with on hover effect

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,43 @@
-import "./Portfolio.scss"
+import './Portfolio.scss'
 
 export default function portfolio() {
   return (
-    <div className='portfolio' id="portfolio">portfolio</div>
+    <div className='portfolio' id='portfolio'>
+      <h1>Portfolio</h1>
+      <ul>
+        <li className='active'>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+
+      <div className='container'>
+        <div className='item'>
+          <img src='assets/analysis-page.jpg' alt='' />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src='assets/analysis-page.jpg' alt='' />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src='assets/analysis-page.jpg' alt='' />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src='assets/analysis-page.jpg' alt='' />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src='assets/analysis-page.jpg' alt='' />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src='assets/analysis-page.jpg' alt='' />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
   )
 }
